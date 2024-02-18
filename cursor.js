80baa7a7849fc25487448e64924c343275f90b2b
James Aliev <james.aliev@gmail.com>
2024-02-18 18:23:54 -0500

    cursor now stretches with movements

--- cursor.js
+++ cursor.js
@@ -8,12 +8,19 @@ const speed = 0.1; // between 0 and 1
 const updatePosition = () => {
   pos.x += (mouse.x - pos.x - cursor.offsetWidth / 2) * speed;
   pos.y += (mouse.y - pos.y - cursor.offsetHeight / 2) * speed;
-  cursor.style.transform = 'translate3d(' + pos.x + 'px ,' + pos.y + 'px, 0)';
+
+  const distanceX = Math.abs(mouse.x - pos.x - cursor.offsetWidth / 2);
+  const distanceY = Math.abs(mouse.y - pos.y - cursor.offsetHeight / 2);
+
+  const stretchFactorX = 1 + distanceX * 0.005;
+  const stretchFactorY = 1 + distanceY * 0.005;
+
+  cursor.style.transform = `translate3d(${pos.x}px, ${pos.y}px, 0) scaleX(${stretchFactorX}) scaleY(${stretchFactorY})`;
 };
 
 const updateCoordinates = e => {
-  mouse.x = e.pageX; // Use pageX instead of clientX
-  mouse.y = e.pageY; // Use pageY instead of clientY
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
 };
 
 const updateSize = () => {
@@ -22,9 +29,10 @@ const updateSize = () => {
 };
 
 const shrink = () => {
-    inner_cursor.style.width = 2 + 'vw';
-    inner_cursor.style.height = 2 + 'vw';
-}
+  inner_cursor.style.width = 2 + 'vw';
+  inner_cursor.style.height = 2 + 'vw';
+};
+
 document.addEventListener('mousedown', updateSize);
 document.addEventListener('mouseup', shrink);
 
