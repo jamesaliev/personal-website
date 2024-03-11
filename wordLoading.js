2c7d81f4a0476e51960ab64c09c8c75497c6bd6b
James Aliev <james.aliev@gmail.com>
2024-03-10 20:12:49 -0400

    Added text animations to a couple things when you hover over them

---
+++ wordLoading.js
@@ -0,0 +1,15 @@
+const letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+document.querySelector(".wordLoading").onmouseover = event => {
+  let iterations = 0;
+  const interval = setInterval(() => {
+     event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+       if (index < iterations) {
+         return event.target.dataset.value[index];
+       }
+       return letters[Math.floor(Math.random() * 52)]
+     }).join("");
+    if (iterations >= event.target.dataset.value.length) clearInterval(interval);
+    iterations += 1;
+  }, 30)
+}
